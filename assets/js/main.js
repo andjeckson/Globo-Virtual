ff1055ecede039cbb15612a339673d046ca806eb
Andjeckson <79026494+andjeckson@users.noreply.github.com>
2025-05-09 10:30:25 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,127 +1,156 @@
-$ = document.querySelector.bind(document)
+let $ = document.querySelector.bind(document) // cria um seletor $()
 
-let globo         = $('.globo')
-let elementoPedra = globo.querySelector('.pedra-sorteada')
-let pedraNumero   = elementoPedra.querySelector('.numero')
-let pedraLetra    = elementoPedra.querySelector('.letra')
-let cedulas     = $('.cedulas')
+let globo = $('#globo')
+let elmPedraSorteada      = $('#globo #pedra-sorteada')
+let elmNumPedraSorteada   = $('.globo .numero')
+let elmLetraPedraSorteada = $('.globo .letra');
+let btnSortear            = $('#btn-sortear')
+let tabelaDeNumeros       = $('#tabela-de-numeros')
+let checkbox              = $('.app-bar input[type=checkbox]')
 
-let pedrasSorteadas = new Set()
-const LIMITE_DE_PEDRAS = 75 // Limite de pedras a serem sorteadas.
-let ultimaPedraSorteada = ''
+let pedrasSorteadas  = new Set()
+let ultimaPedraSorteada = null
+let sinteseDeVoz = false
 
+// Adicona zero à esquerda aos números menores que 10
 
-function criarTabela(){
-    const table = document.createElement('table')
+function adicionarZeroAEsquerda(numero){
+    let stringNumero = String(numero)
+    let numeroFormatado = stringNumero.padStart(2, '0')
+        return numeroFormatado
+}
+
+//Gira o globo
+function girarOGlobo(){
+    globo.classList.add('girar')
+    btnSortear.disabled = true
+    falar('Sorteando pedra…')
+}
+
+globo.onanimationend = ()=>{
+    globo.classList.remove('girar')
+    btnSortear.disabled = false
+     sortearPedra()
+}
+
+//Gera uma tabela com os números de 1 a 75
+function gerarTabelaDeNumeros(){
     const thead = document.createElement('thead')
     const tbody = document.createElement('tbody')
     
-    const headRow = document.createElement('tr')
+    tabelaDeNumeros.appendChild(thead)
+    tabelaDeNumeros.appendChild(tbody)
+    
+    const tr = document.createElement('tr')
+          thead.appendChild(tr)
+    
     const BINGO = ['B','I','N','G','O']
+          BINGO.forEach((letra)=>{
+              const th = document.createElement('th')
+                    th.textContent = letra
+                    th.className = letra
+                    
+              tr.appendChild(th)
+          })
+          
+          // Gera os números de 1 a 75
+          
+          for( let i = 1; i <= 15; i++){
+               const tr = document.createElement('tr')
+                     for( j = 0; j < 5; j++){
+                         let n = j === 0 ? i : i + (15*j)
+                         let cedula = criarCedula(n, BINGO[j])
+                         tr.appendChild(cedula)
+                     }
+                     
+                     tbody.appendChild(tr)
+          }
     
-    BINGO.forEach((letra)=>{
-         const th = document.createElement('th')
-               th.innerHTML = letra
-               headRow.appendChild(th)
-    })
-    
-    thead.appendChild(headRow)
-    
-    table.appendChild(thead)
-    
-    for(let i = 1 ;i <= 15; i++){
-        let tr = document.createElement('tr')
+}
+
+// cria cedula da tabela com os respectivos números 
+
+function criarCedula(numero, letra){
+     const td = document.createElement('td')
+           td.textContent = adicionarZeroAEsquerda(numero)
+           td.id = 'pedra-' + numero
+           td.classList = 'pedra-'+letra.toLowerCase()
+           td.dataset.letra = letra
+           
+           return td
+}
+
+gerarTabelaDeNumeros()
+
+
+// Sorteia uma pedra
+function sortearPedra(){
+     const numero = Math.floor( Math.random() * 75) + 1
+           
+           // Evita que os números se repitam
+           if( pedrasSorteadas.has(numero)){
+                 sortearPedra() // Sorteia novamente
+           }else{
+               pedrasSorteadas.add(numero) // Adiciona a pedra
+               mostrarPedraSorteada(numero)
+           }
+}
+
+
+// Mostra a ultima pedra sorteada
+function mostrarPedraSorteada(numero){
+
+    let cedula = $('#pedra-'+numero)
+        cedula.classList.add('pintar')
         
-        let tdB = document.createElement('td')
-            tdB.id = 'pedra-'+ i
-            tdB.textContent = addZeroAEsquerda(i)
-            tdB.dataset.letra = 'B'
-            tr.appendChild(tdB)
-            
-         let tdI = document.createElement('td')
-             tdI.textContent = i + 15
-             tdI.id = 'pedra-'+ (i + 15)
-             tdI.dataset.letra = 'I'
-             tr.appendChild(tdI)
-             
-         let tdN = document.createElement('td')
-            tdN.textContent =  i + 30
-            tdN.id = 'pedra-'+ (i + 30)
-            tdN.dataset.letra = 'N'
-            tr.appendChild(tdN)
-            
-         let tdG = document.createElement('td')
-             tdG.textContent =  i + 45
-             tdG.id = 'pedra-'+ (i + 45)
-             tdG.dataset.letra = 'G'
-             tr.appendChild(tdG)
-             
-         let tdO = document.createElement('td')
-             tdO.textContent = i + 60
-             tdO.id = 'pedra-'+ (i+60)
-             tdO.dataset.letra = 'O'
-             tr.appendChild(tdO)
-         
-       tbody.appendChild(tr)
+    let letra = cedula.dataset.letra
+    
+    elmPedraSorteada.className = 'pedra-sorteada pedra-'+String(letra).toLowerCase()
+    
+    elmLetraPedraSorteada.textContent = letra
+    elmNumPedraSorteada.textContent = adicionarZeroAEsquerda(numero);
+    
+    let fala = `Pedra sorteada: letra ${letra != 'O' ? letra: 'Ó'}, número ${numero}. ${letra != 'O' ? letra: 'Ó'}, ${numero}`
+    falar(fala)
+}
+
+
+// Usa a tts para falar as pedras sorteadas
+function falar(texto){
+    if( sinteseDeVoz === true)
+    {
+           
+     let speechUtterance = new SpeechSynthesisUtterance(texto)
+         speechUtterance.lang = 'pt-BR'
+         speechUtterance.rate = 1.5
+         speechSynthesis.speak( speechUtterance )
+ }
+}
+
+checkbox.addEventListener('change',()=>{
+     sinteseDeVoz = checkbox.checked
+    
+    if( window.SpeechSynthesisUtterance){
+         if( sinteseDeVoz === true){
+              falar('Síntese de voz ativada!')
+         }
+    }else{
+        alert('Seu navegador não tem suporte para a síntese de voz.')
+        checkbox.checked = false
     }
-    table.appendChild(tbody)
-    cedulas.appendChild(table)
-    
-}
-
-
-
-function sortearPedra() {
-    
-    let pedra = Math.floor(Math.random() * LIMITE_DE_PEDRAS) + 1;
-    
-    // Remove pedra repetida
-    if (pedrasSorteadas.has(pedra)) {
-        return sortearPedra(); // Faz um novo sorteio
-    } else {
-        pedrasSorteadas.add(pedra); 
-        ultimaPedraSorteada = pedra;
-    }
-}
-
-
-
-function girarOGlobo(){
-    globo.classList.add('girar')
-    elementoPedra.classList.remove('mostrar')
-}
-
-globo.addEventListener('click', girarOGlobo)
-
-globo.addEventListener('animationend', ()=>{
-  sortearPedra()
-  mostrarPedraSorteada()
 })
 
-function mostrarPedraSorteada(){
- let cedula = cedulas.querySelector('#pedra-' + ultimaPedraSorteada)
- 
-     cedula.classList.add('sorteada');
-     pedraLetra.innerHTML = cedula.dataset.letra
-     pedraNumero.innerHTML = addZeroAEsquerda(ultimaPedraSorteada)
-         
-         elementoPedra.classList.add('mostrar')
-         globo.classList.remove('girar')
+btnSortear.onclick = ()=>{
+      girarOGlobo()
 }
 
 
-
-function addZeroAEsquerda(str){
-     return String(str).padStart(2, '0')
-}
-
-criarTabela()
-
+// Abertura do app
 
 let telaDeAbertura = document.querySelector('.abertura')
-    telaDeAbertura.onanimationend = (e)=>{
-        if(e.target.classList.contains('abertura'))
-        {
+    telaDeAbertura.addEventListener('animationend', (e)=>{
+           if(e.target.classList.contains('abertura'))
+           {
              telaDeAbertura.remove()
-        }
-}
+           }
+    })
